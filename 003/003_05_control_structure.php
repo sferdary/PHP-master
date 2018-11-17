8d7cc948f1f21cff273899dba3f0a7b68644d73f
sferdary <s.ferdary@pm.me>
2018-11-17 20:34:10 +0100

    php exercises

--- 003/003_05_control_structure.php
+++ 003/003_05_control_structure.php
@@ -25,32 +25,4 @@ for ($i = 1; $i <=9 ;$i++){
 }
  ?>
 </body>
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: S. Ferdary
- * Date: 15/11/2018
- */
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>003_05</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body class="centered_text">
-<?php
-for ($i = 1; $i <=9 ;$i++){
-    if($i == 3){
-        $red_border = "class='red_border'";
-    }else{
-        $red_border = "";
-    }
-    echo "<img $red_border  src='img/monkeys/aap$i.jpg'>";
-}
- ?>
-</body>
 </html>
--- 003/003_06_modulus_operator.php
+++ 003/003_06_modulus_operator.php
@@ -25,32 +25,4 @@ for ($i = 1; $i <=9 ;$i++){
 }
  ?>
 </body>
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: S. Ferdary
- * Date: 15/11/2018
- */
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>003_05</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body class="centered_text">
-<?php
-for ($i = 1; $i <=9 ;$i++){
-    if($i % 2 == 0){
-        $border = "class='red_border'";
-    }else{
-        $border = "class='green_border'";
-    }
-    echo "<img $border  src='img/monkeys/aap$i.jpg'>";
-}
- ?>
-</body>
 </html>
